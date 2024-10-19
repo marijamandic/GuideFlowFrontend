20b84cf10493f19528c729df2191364da510c1a0
Aleksa <kozicaleksa02@gmail.com>
2024-10-19 18:43:30 +0200

    fix: Added markdown support

--- Explorer/src/app/feature-modules/blog/blog.module.ts
+++ Explorer/src/app/feature-modules/blog/blog.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { PostComponent } from './post/post.component';
 import { CreateBlogComponent } from './create-blog/create-blog.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MarkdownModule } from 'ngx-markdown';
 
 
 
@@ -13,7 +14,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MarkdownModule.forRoot()
   ],
   exports: [PostComponent , CreateBlogComponent]
 })
