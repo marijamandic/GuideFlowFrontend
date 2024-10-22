93af80c516343989211bc0b05690f3f7b6bc14a7
reljajocic <149072501+reljajocic@users.noreply.github.com>
2024-10-22 17:25:05 +0200

    fix: spacing and sizing improvement.

--- Explorer/src/app/feature-modules/layout/home/home.component.ts
+++ Explorer/src/app/feature-modules/layout/home/home.component.ts
@@ -14,12 +14,12 @@ export class HomeComponent implements OnInit {
   currentImageIndex: number = 0;
 
   tours = [
-    { name: 'Tour 1', description: 'Explore the mountains.', rating: 8.5, reviews: 'Very good, 235 reviews', imageUrl: 'assets/images/tour1.jpg' },
-    { name: 'Tour 2', description: 'Explore the desert', rating: 9.0, reviews: 'Excellent, 150 reviews', imageUrl: 'assets/images/tour2.jpg' },
-    { name: 'Tour 3', description: 'Discover the city.', rating: 7.5, reviews: 'Good, 100 reviews', imageUrl: 'assets/images/tour3.jpg' },
-    { name: 'Tour 4', description: 'Experience the jungle.', rating: 8.0, reviews: 'Very good, 180 reviews', imageUrl: 'assets/images/tour1.jpg' },
-    { name: 'Tour 5', description: 'Relax at the beach.', rating: 7.5, reviews: 'Good, 100 reviews', imageUrl: 'assets/images/tour2.jpg' },
-    { name: 'Tour 6', description: 'Experience the jungle.', rating: 8.0, reviews: 'Very good, 180 reviews', imageUrl: 'assets/images/tour3.jpg' }
+    { name: 'Lorem Ipsum', description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ', rating: 8.5, reviews: 'Very good, 235 reviews', imageUrl: 'assets/images/tour1.jpg' },
+    { name: 'Lorem Ipsum', description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ', rating: 9.2, reviews: 'Excellent, 150 reviews', imageUrl: 'assets/images/tour2.jpg' },
+    { name: 'Lorem Ipsum', description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ', rating: 7.5, reviews: 'Good, 100 reviews', imageUrl: 'assets/images/tour3.jpg' },
+    { name: 'Lorem Ipsum', description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ', rating: 8.1, reviews: 'Very good, 180 reviews', imageUrl: 'assets/images/tour1.jpg' },
+    { name: 'Lorem Ipsum', description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ', rating: 7.5, reviews: 'Good, 100 reviews', imageUrl: 'assets/images/tour2.jpg' },
+    { name: 'Lorem Ipsum', description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ', rating: 8.3, reviews: 'Very good, 180 reviews', imageUrl: 'assets/images/tour3.jpg' }
   ];
 
   ngOnInit() {
@@ -34,6 +34,12 @@ export class HomeComponent implements OnInit {
 
   ngAfterViewInit() {
     const container = document.querySelector('.tour-cards-container') as HTMLElement;
+
+    // Add event listener for mouse wheel scrolling
+    container.addEventListener('wheel', (e) => {
+      e.preventDefault(); // Prevent the default vertical scroll behavior
+      container.scrollLeft += e.deltaY; // Scroll horizontally based on vertical scroll
+    });
 
     let isDown = false;
     let startX: number;
