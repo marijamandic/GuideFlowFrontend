2b4272239f3a154f0ca3b8294b9c72a17e5016ee
ra186-2021 <janicevicivan90@gmail.com>
2024-10-23 20:13:17 +0200

    changed: create-object-form

--- Explorer/src/app/feature-modules/tour-authoring/tour-object/tour-object.component.ts
+++ Explorer/src/app/feature-modules/tour-authoring/tour-object/tour-object.component.ts
@@ -15,21 +15,37 @@ export class TourObjectComponent implements OnInit {
   tourObjects: TourObject[] = [];
   shouldRenderTourObjectForm: boolean = false;
   shouldEdit: boolean = false;
+  latitude: number | null = null;
+  longitude: number | null = null;
+  selectedTourObject: any = null;
   
   constructor(private service: TourAuthoringService) { }
 
   ngOnInit(): void {
     this.getTourObjects();
   }
-
+  onCoordinatesSelected(coordinates: { latitude: number; longitude: number }): void {
+    console.log('Coordinates selected:', coordinates);
+    this.latitude = coordinates.latitude;
+    this.longitude = coordinates.longitude;
+  }
   onMarkerAdded(latlng: L.LatLng): void {
     console.log('New marker added at:', latlng);
-    // Logika za dodavanje nove ključne tačke ili objekta
   }
+
+  onEditClicked(tourObject: any): void {
+    this.selectedTourObject = tourObject;
+    console.log(this.selectedTourObject)
+    this.latitude = tourObject.latitude;
+    this.longitude = tourObject.longitude;
+    this.shouldEdit = true; // Oznaka da je forma u modu uređivanja
+    this.shouldRenderTourObjectForm = true; // Prikaz forme za uređivanje
+  }
+  
+
 
   onMapReset(): void {
     console.log('Map reset');
-    // Logika za resetovanje ili ažuriranje stanja komponente
   }
 
   getTourObjects(): void {
