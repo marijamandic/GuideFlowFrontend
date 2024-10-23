a1e90ea38d00ca86e8bf214767558f9e5d7b6dc7
Mihailo84 <mihailo.vukovic2002@gmail.com>
2024-10-23 14:02:08 +0200

    sitne izmene

--- Explorer/src/app/feature-modules/layout/rating-the-app/rating-the-app.component.ts
+++ Explorer/src/app/feature-modules/layout/rating-the-app/rating-the-app.component.ts
@@ -1,24 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppRating } from '../model/AppRating.model';
 import { AppRatingService } from '../layout.service';
+import { User } from 'src/app/infrastructure/auth/model/user.model';
+import { AuthService } from 'src/app/infrastructure/auth/auth.service';
 
 @Component({
   selector: 'rating-the-app',
   templateUrl: './rating-the-app.component.html',
   styleUrls: ['./rating-the-app.component.css']
 })
-export class RatingTheAppComponent {
+export class RatingTheAppComponent implements OnInit {
   rating: AppRating = {
     ratingId: 3,
     userId: 6,
     ratingValue: 0,
-    comment: '',
+    comment: ' ',
     ratingTime: new Date().toISOString()
   };
 
+  user: User | undefined;
   submitted: boolean = false;  
 
-  constructor(private appRatingService: AppRatingService) {}
+  constructor(
+    private appRatingService: AppRatingService,
+    private authService: AuthService 
+  ) {}
+
+  ngOnInit(): void {
+    this.authService.user$.subscribe(user => {
+      this.user = user;
+
+      if (user) {
+        this.rating.userId = user.id; 
+        console.log(user.username);
+      }
+    });
+  }
 
   onSubmit() {
     this.appRatingService.postNewAppRating(this.rating).subscribe(
