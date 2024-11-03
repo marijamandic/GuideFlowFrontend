1854b5d5680d503d760190e811b88bac36136e3b
spasojevic-nikola <148842103+spasojevic-nikola@users.noreply.github.com>
2024-11-03 20:45:43 +0100

    Update administration.module.ts

--- Explorer/src/app/administration/administration.module.ts
+++ Explorer/src/app/administration/administration.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EquipmentComponent } from './equipment/equipment.component';
-import { ProfileInfoComponent } from '../feature-modules/administration/profile-info/profile-info.component';
 import { HomeComponent } from '../layout/home/home.component';
 
 @NgModule({
