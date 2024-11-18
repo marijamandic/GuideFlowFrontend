02a0760a3285aa22577f6da0db9e2602aebab97f
Vasilije Kiso <kisovasilije46@gmail.com>
2024-11-18 23:57:56 +0100

    feat: remove item from cart

--- Explorer/src/app/feature-modules/marketplace/shopping-cart/shopping-cart.component.ts
+++ Explorer/src/app/feature-modules/marketplace/shopping-cart/shopping-cart.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ShoppingCart } from '../model/shopping-carts/shopping-cart';
 import { MarketplaceService } from '../marketplace.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { SingleItem } from '../model/shopping-carts/single-item';
 
 @Component({
 	selector: 'xp-shopping-cart',
@@ -15,18 +16,34 @@ export class ShoppingCartComponent implements OnInit {
 		singleItems: []
 	};
 
+	totalAdventureCoins = 0;
+
 	constructor(private marketplaceService: MarketplaceService) {}
 
-	calculateTotalPrice(): number {
-		let adventureCoin = 0;
-		for (let item of this.cart.singleItems) adventureCoin += item.adventureCoin;
-		return adventureCoin;
+	calculateAc(items: SingleItem[]): void {
+		this.totalAdventureCoins = 0;
+		items.forEach(i => {
+			this.totalAdventureCoins += i.adventureCoin;
+		});
+	}
+
+	handleRemoveClick(singleItemId: number): void {
+		this.marketplaceService.removeFromCart(singleItemId).subscribe({
+			next: (): void => {
+				this.cart.singleItems = [...this.cart.singleItems.filter(i => i.id !== singleItemId)];
+				this.calculateAc(this.cart.singleItems);
+			},
+			error: (err: HttpErrorResponse): void => {
+				console.log(err.message);
+			}
+		});
 	}
 
 	ngOnInit(): void {
 		this.marketplaceService.getShoppingCartByTouristId().subscribe({
 			next: (result: ShoppingCart) => {
 				this.cart = result;
+				this.calculateAc(this.cart.singleItems);
 			},
 			error: (err: HttpErrorResponse) => {
 				console.log(err.message);
