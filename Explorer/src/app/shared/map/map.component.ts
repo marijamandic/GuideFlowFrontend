e7114c6a35fc2d0ca8ec83ec6dac161a18cdb3dd
Aleksa <kozicaleksa02@gmail.com>
2024-11-24 16:09:09 +0100

    fix: Fixed map component

--- Explorer/src/app/shared/map/map.component.ts
+++ Explorer/src/app/shared/map/map.component.ts
@@ -108,10 +108,10 @@ export class MapComponent implements AfterViewInit,OnChanges {
   ngOnChanges(): void {
     if (this.map) {
       this.updateCheckpointMarkers();
-      this.updateEncounterMarkers();
-    } /*else {
+      //this.updateEncounterMarkers();
+    } else {
       this.map.invalidateSize();
-    }*/
+    }
   }
 
   private updateCheckpointMarkers(): void {
@@ -127,16 +127,16 @@ export class MapComponent implements AfterViewInit,OnChanges {
       const waypoints = this.markers.map(m => m.getLatLng());
       this.setRoute(waypoints);
     }
-  }
-
-  private updateEncounterMarkers(): void {
-    this.resetMap();
-
     this.encounters.forEach((encounter) => {
       const latLng = new L.LatLng(encounter.latitude, encounter.longitude);
       const marker = L.marker(latLng).addTo(this.map);
       this.addMarker(marker);
     });
+  }
+
+  private updateEncounterMarkers(): void {
+    this.resetMap();
+
   }
 
 
