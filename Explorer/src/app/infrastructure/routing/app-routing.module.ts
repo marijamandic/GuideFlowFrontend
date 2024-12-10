886c07c4c536edaf43d0fba1416a4c46211016de
Aleksa <kozicaleksa02@gmail.com>
2024-12-10 18:19:25 +0100

    fixed encounter module

--- Explorer/src/app/infrastructure/routing/app-routing.module.ts
+++ Explorer/src/app/infrastructure/routing/app-routing.module.ts
@@ -121,6 +121,7 @@ const routes: Routes = [
 
   // ## Encounter
   { path: 'encounters', component: EncounterComponent},
+  {path: 'encounters/:encounterExecutionId/:tourExecutionId' , component:EncounterComponent},
   { path: 'encounter-add', component: EncounterFormComponent },
   { path: 'encounter-update/:id', component: EncounterFormComponent },
   { path: 'author-add-encounter/:id/:tourId', component: AddEncounterComponent},
